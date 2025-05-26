c2c4faccee27c15cae0fd1617cab4715c58febd4
Sriilekhaa <foreverrlearnerr@gmail.com>
2025-05-27 00:20:35 +0530

    ios

--- safe_app/ios/Runner/AppDelegate.swift
+++ safe_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Initialize Google Maps before Flutter engine
+    GMSServices.provideAPIKey("AIzaSyCroP5ArTzF4g5GmZdr7ml9KDlRviEQfbE")
+    _ = GMSServices.sharedServices() // Force initialization
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
