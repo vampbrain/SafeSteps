5c45f8f0f0cc1114aad2bdf2cb4feddcc17a86e4
Yogesh <117334483+Yogesh-005@users.noreply.github.com>
2025-05-26 11:54:11 +0530

    REMOVED MAP API KEI

--- safe_app/lib/utils/constants.dart
+++ safe_app/lib/utils/constants.dart
@@ -1,6 +1,6 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class AppConstants {
-  static const String googleMapsApiKey = 'AIzaSyCroP5ArTzF4g5GmZdr7ml9KDlRviEQfbE';
+  static const String googleMapsApiKey = 'GOOFLE_MAP_APIKEY';
   static const LatLng electronicCityCenter = LatLng(12.8487599, 77.6482529582762);
 }
